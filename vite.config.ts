7625240cf14070e501a7fb27e9adc11bf75478bd
Romuald Brillout <git@brillout.com>
2024-06-27 23:11:32 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import ssr from "vike/plugin";
+import react from "@vitejs/plugin-react";
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [devServer({
+    entry: "hono-entry.ts",
+
+    exclude: [
+      /^\/@.+$/,
+      /.*\.(ts|tsx|vue)($|\?)/,
+      /.*\.(s?css|less)($|\?)/,
+      /^\/favicon\.ico$/,
+      /.*\.(svg|png)($|\?)/,
+      /^\/(public|assets|static)\/.+/,
+      /^\/node_modules\/.*/,
+    ],
+
+    injectClientScript: false,
+  }), react({}), ssr({})],
+});
